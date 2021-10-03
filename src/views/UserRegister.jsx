65f8aeb1d30ae67d22b42d3a473e899afcb54c8a
小柳梨華 <koyanagirika@yagibukku.local>
2021-10-03 19:04:41 +0900

    divを変更

--- src/views/UserRegister.jsx
+++ src/views/UserRegister.jsx
@@ -15,22 +15,22 @@ const UserRegister = () => {
                 <body>
                     <h3>あなたの情報を登録しましょう</h3>
 
-                    <div class="col-5 mx-auto">
+                    <div className="col-5 mx-auto">
                         <label for="Input" class="form-label">ユーザー名</label>
-                        <div class="input-group flex-nowrap">
-                            <span class="input-group-text" id="addon-wrapping">@</span>
+                        <div className="input-group flex-nowrap">
+                            <span className="input-group-text" id="addon-wrapping">@</span>
                             <input type="text" class="form-control" placeholder="ユーザー名" aria-label="ユーザー名" aria-describedby="addon-wrapping" />
                         </div>
                     </div>
                     <br />
-                    <div class="col-5 mx-auto">
+                    <div className="col-5 mx-auto">
                         <label for="Input" class="form-label">フリーコメント</label>
                         <input type="text" class="form-control" id="Input" />
                     </div>
 
 
 
-                    <center><i class="fas fa-arrow-circle-right fa-3x"></i></center>
+                    <center><i className="fas fa-arrow-circle-right fa-3x"></i></center>
 
 
                 </body>
