bdf9be51f4e76f73c39513f69686465b9fc4845f
sugimoto-ecn <kein@encinc.com>
2021-10-03 19:35:11 +0900

    Bタスクの準備

---
+++ src/views/dashboard/SleepData/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from "./SleepData.module.css"
+import DefaultLayout from '../../../components/templates/DefaultLayout'
+
+const SleepData = () => {
+    return (
+        <div>
+            <DefaultLayout>
+                <>
+                <div>
+                    C-1
+                    睡眠データ
+                </div>
+                </>
+            </DefaultLayout>
+        </div>
+    )
+}
+
+export default SleepData
