65f8c4d916d916755834011e0ff73310ef0eab49
小柳梨華 <koyanagirika@yagibukku.local>
2021-10-04 18:57:13 +0900

    確認お願いします。

--- src/components/organisms/TopBar/index.jsx
+++ src/components/organisms/TopBar/index.jsx
@@ -1,29 +1,38 @@
 import React from 'react'
 import styles from './TopBar.module.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Dropdown } from 'react-bootstrap';
+
 
 const TopBar = () => {
     return (
         <div>
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">  <img src="..." alt="サービス名の画像" width="30" height="30" /></a>
-                    <form class="d-flex mr-auto">
-                        {/*友達検索*/}
-                        <input type="search" class="form-control me-sm-2" placeholder="友達を検索する" aria-label="検索..." />
-                        <button type="submit" class="btn btn btn-outline-secondary flex-shrink-0">検索</button>
-                    </form>
-                    {/*ドロップダウンメニュー */}
-                    <div className="d-flex ml-auto">
-                        <div className="dropdown">
-                            <a className="btn" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
-                                <i className="fas fa-user-cog fa-2x " style={{ color: '#666666' }}></i>
-                            </a>
-                            <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                                <li><a className="dropdown-item" href="#">メニュー1</a></li>
-                                <li><a className="dropdown-item" href="#">メニュー2</a></li>
-                                <li><a className="dropdown-item" href="#">メニュー3</a></li>
-                            </ul>
+                    <div className="row">
+                        <div className="col col-5">
+                            <a class="navbar-brand" href="#"> <img src="..." alt="サービス名の画像" width="30" height="30" /></a>
                         </div>
+                        <div className="col col-7">
+                            <form class="d-flex ">
+                                {/*友達検索*/}
+                                <input type="search" class="form-control me-sm-2" placeholder="友達を検索する" aria-label="検索" />
+                                <button type="submit" class="btn btn btn-outline-secondary flex-shrink-0">検索</button>
+                            </form>
+                        </div>
+                    </div>
+                    <div className="row justify-content-end">
+                        {/* ドロップダウンメニュー */}
+                        <Dropdown>
+                            <Dropdown.Toggle variant="light">
+                                <i className="fas fa-user-cog fa-2x " style={{ color: '#666666' }}></i>
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
                     </div>
                 </div>
             </nav>
