c3e57a9ea6d321dd4b57852745aad8c736763fd7
石塚千尋 <ne191238@senshu-u.jp>
2021-10-04 16:41:20 +0900

    タイトル〜ログイン close #2

--- src/views/Top.jsx
+++ src/views/Top.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React from 'react';
+import './Top.module.css';
+
 
 const Top = () => {
-    return (
-        <div>
-            
+    return ( 
+        
+        <center>
+ 
+        <div className="t1">
+          <h1>一緒におやすみ</h1>
         </div>
+        <div className="t2">
+          <h2>~ともに実現する快適LIFE~</h2>
+        </div>
+            <br/>
+        <div className="t3">
+        新規登録
+        </div>
+            <br/> 
+        
+        
+        <div className="box1">
+        <button type="button" className="btn btn-primary rounded-pill">Googleアカウントで登録</button>
+        </div>
+        <br/>
+        <div className="box2">
+        <button type="button" className="btn btn-primary rounded-pill">メールアドレスで登録</button>
+        </div>
+        <br/>
+        <div className="box1">
+        <button type="button" className="btn btn-primary rounded-pill">ログイン</button>
+        </div>
+        
+        </center>
+            
     )
 }
 
