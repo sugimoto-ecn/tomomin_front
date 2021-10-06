405d949ce09d963988bdf1700bb1ec1729a2c164
湯浅慶一 <yuasakeiichi@yuasakeiichinoMacBook.local>
2021-10-06 12:24:54 +0900

    A-1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import Router from './Router'
+import Button from '@material-ui/core/Button';
+
 
 const App = () => {
   return (
+  <div>
+    <Button variant="contained" color="primary" float="right">
+      ログイン
+    </Button>
     <Router/>
+  </div>
   )
 }
 
--- src/views/LP.jsx
+++ src/views/LP.jsx
@@ -5,10 +5,18 @@ import './LP.module.css'
 const LP = () => {
     console.log("LP")
     return (
-        <div className="text-danger">
-            A-1LP画面 練習中
+        <div>
+        <div className="out">
+            サービス名
+        </div>
+        <a class="navbar-brand" href="#"> <img src="logo111.png" alt="画像" width="1000" height="800" /></a>
+        <h1>
+            概要　　　　　　　　
+        </h1>
         </div>
     )
 }
 
+
 export default LP
+
