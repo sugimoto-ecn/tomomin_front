f8d6758de7961f5efa141100222143662ec26aeb
湯浅慶一 <yuasakeiichi@yuasakeiichinoMacBook.local>
2021-10-08 11:47:46 +0900

    これでできていますか

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react'
 import Router from './Router'
 import Button from '@material-ui/core/Button';
 
-
 const App = () => {
   return (
   <div>
+
     <Button variant="contained" color="primary" float="right">
       ログイン
     </Button>
     <Router/>
+
   </div>
   )
 }
--- src/views/LP.jsx
+++ src/views/LP.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
-import './LP.module.css'
+import styles from './LP.module.css'
+
 
 
 const LP = () => {
     console.log("LP")
     return (
         <div>
-        <div className="out">
-            サービス名
-        </div>
-        <a class="navbar-brand" href="#"> <img src="logo111.png" alt="画像" width="1000" height="800" /></a>
-        <h1>
-            概要　　　　　　　　
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                    <div className="row">
+                        <div className="col col-5">
+                            <a class="navbar-brand" href="#"> <img src="..." alt="サービス名の画像" width="30" height="30" /></a>
+                        </div>
+                        </div>
+
+                         <div className="row justify-content-end">
+<button type="button" class="btn page-link link-dark">ログイン</button>
+                    </div>
+                    </div>
+                    </nav>
+                    <a class="navbar-brand" className={`${styles.test}`} href="#"> <img src="logo111.png" alt="画像" className={`${styles.test}`} /></a>
+                    <h1>
+                        概要
         </h1>
+       
         </div>
     )
 }
