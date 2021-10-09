d48d2234874591bfd7ab9b98e95d7616d269ff5a
sugimoto-ecn <kein@encinc.com>
2021-10-09 14:41:32 +0900

    不要コード削除

--- src/App.js
+++ src/App.js
@@ -5,12 +5,7 @@ import Button from '@material-ui/core/Button';
 const App = () => {
   return (
   <div>
-
-    <Button variant="contained" color="primary" float="right">
-      ログイン
-    </Button>
     <Router/>
-
   </div>
   )
 }
