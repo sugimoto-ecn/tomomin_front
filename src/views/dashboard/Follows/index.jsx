1983c713619230b1ba259a46fb93214bdabb7e72
sugimoto-ecn <kein@encinc.com>
2021-10-09 14:40:05 +0900

    c作業準備

---
+++ src/views/dashboard/Follows/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from './Follows.module.css'
+import DefaultLayout from '../../../components/templates/DefaultLayout'
+
+const Follows = () => {
+    return (
+        <div>
+           <DefaultLayout>
+                <>
+                <div>
+                    C-3
+                </div>
+                </>
+            </DefaultLayout>
+        </div>
+    )
+}
+
+export default Follows
