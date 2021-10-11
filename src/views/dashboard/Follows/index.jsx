c3b76e736025f874d75587ffbb3545109c405f4a
湯浅慶一 <yuasakeiichi@yuasakeiichinoMacBook.local>
2021-10-12 00:02:02 +0900

    修正中

---
+++ src/views/dashboard/Follows/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from './Follows.module.css'
+import DefaultLayout from '../../../components/templates/DefaultLayout'
+
+const Follows = () => {
+
+    return (
+        <div>
+           <DefaultLayout>
+                <>
+                <div>
+                    アイウエオ
+                </div>
+                </>
+            </DefaultLayout>
+        </div>
+    )
+}
+
+export default Follows
