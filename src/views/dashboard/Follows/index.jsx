452d75e04b6e68d26ee969798632576a905a26e6
sugimoto-ecn <kein@encinc.com>
2021-10-15 10:51:30 +0900

    マージ作業

--- src/views/dashboard/Follows/index.jsx
+++ src/views/dashboard/Follows/index.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import styles from './Follows.module.css'
 import DefaultLayout from '../../../components/templates/DefaultLayout'
+// import { Switch } from 'react-neumorphic-toggle';
+// console.log(Switch)
+import Switch from "react-switch";
+
 
 const Follows = () => {
     return (
@@ -9,6 +13,8 @@ const Follows = () => {
                 <>
                 <div>
                     C-3
+                    {/* <Switch /> */}
+                    <Switch onChange={() => console.log('test')} checked={() => console.log("ijj")} />
                 </div>
                 </>
             </DefaultLayout>
